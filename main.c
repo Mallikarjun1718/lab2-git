5402f2b4359358b809f7dccc1f227506d3c90dac
Mallikarjun S Biradar <220020034@iitdh.ac.in>
2025-09-08 11:42:59 +0530

    lab5 code added

--- main.c
+++ main.c
@@ -3,56 +3,25 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 
-#define RED_LED (1<<1)
-#define BLUE_LED (1<<2)
-#define GREEN_LED (1<<3)
-
-int main(void)
-{
-    SYSCTL_RCGC2_R |=   0x00000020;      // ENABLE CLOCK TO GPIOF
+int main (void){
+    SYSCTL_RCGC2_R   |= 0x00000020;      // ENABLE CLOCK TO GPIOF
     GPIO_PORTF_LOCK_R = 0x4C4F434B;      // UNLOCK COMMIT REGISTER
     GPIO_PORTF_CR_R   = 0x1F;            // MAKE PORTF0 CONFIGURABLE
     GPIO_PORTF_DEN_R  = 0x1F;            // SET PORTF PINS 4 PIN
     GPIO_PORTF_DIR_R  = 0x0E;            // SET PORTF4 PIN AS INPUT USER SWITCH PIN
     GPIO_PORTF_PUR_R  = 0x11;            // PORTF4 IS PULLED UP
-    GPIO_PORTF_DATA_R = 0x00;            // LED OFF
+    GPIO_PORTF_DATA_R = 0x02;            // LED OFF
 
-    while(1)
-    {
-        if((GPIO_PORTF_DATA_R & 0x11)==0x01)
-        {
-            delay(10000);
-            GPIO_PORTF_DATA_R |= RED_LED ;
-        }
-        else if((GPIO_PORTF_DATA_R & 0x11)==0x10)
-        {
-            delay(10000);
-            GPIO_PORTF_DATA_R |= BLUE_LED;
-        }
-        else if((GPIO_PORTF_DATA_R & 0x11)==0x00)
-        {
-            delay(10000);
-            GPIO_PORTF_DATA_R |= GREEN_LED;
-        }
-        else
-        {
-            delay(10000);
-            GPIO_PORTF_DATA_R = 0x00;
+    NVIC_ST_RELOAD_R=0x40FFFF;
+    NVIC_ST_CTRL_R=(1<<2);
+    NVIC_ST_CTRL_R|=1;
+    while(1){
+        if((NVIC_ST_CTRL_R & 0x10000)==0x10000){
+            GPIO_PORTF_DATA_R ^=0x02;
+            NVIC_ST_CTRL_R^=1;
+            NVIC_ST_CURRENT_R=0;
+            NVIC_ST_CTRL_R|=1;
         }
     }
 }
 
-
-void delay(int ms){
-    int i,j,count=100;
-    for(i=0;i<ms;i++){
-        for(j=0;j<ms;j++){
-            if(count==0){
-                break;
-                }
-            else{
-                count--;
-            }
-        }
-    }
-}

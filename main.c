67d70238cf9b9d1375fe9bc13223c2bef0042d3d
Mallikarjun S Biradar <220020034@iitdh.ac.in>
2025-09-20 18:40:32 +0530

    lab6 code added

--- main.c
+++ main.c
@@ -1,36 +1,73 @@
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 
-int main (void){
+#define red_led (1<<1)
+
+void timer(void){
+        GPIO_PORTF_DATA_R ^= red_led;
+    }
+int main(void){
     SYSCTL_RCGC2_R   |= 0x00000020;      // ENABLE CLOCK TO GPIOF
     GPIO_PORTF_LOCK_R = 0x4C4F434B;      // UNLOCK COMMIT REGISTER
     GPIO_PORTF_CR_R   = 0x1F;            // MAKE PORTF0 CONFIGURABLE
     GPIO_PORTF_DEN_R  = 0x1F;            // SET PORTF PINS 4 PIN
     GPIO_PORTF_DIR_R  = 0x0E;            // SET PORTF4 PIN AS INPUT USER SWITCH PIN
     GPIO_PORTF_PUR_R  = 0x11;            // PORTF4 IS PULLED UP
-    GPIO_PORTF_DATA_R = 0x00;            // LED OFF
+    GPIO_PORTF_DATA_R = 0x00;
 
     NVIC_ST_RELOAD_R=0xFFFFFF;
-    NVIC_ST_CTRL_R=(1<<2);
-    NVIC_ST_CTRL_R|=1;
-    while(1){
-        if((NVIC_ST_CTRL_R & 0x10000)==0x10000){
-            GPIO_PORTF_DATA_R ^=0x02;
-            NVIC_ST_CTRL_R^=1;
-            NVIC_ST_CURRENT_R=0;
-            NVIC_ST_CTRL_R|=1;
-        }
+    NVIC_ST_CTRL_R=(1<<2 | 1<<1 | 1);
 
-        if((GPIO_PORTF_DATA_R & 0x10)==0x00){
-            GPIO_PORTF_DATA_R|=0x04;
-        }
-        else{
-            GPIO_PORTF_DATA_R&=0x2;
-        }
+    while(1){
     }
 }
+
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "tm4c123gh6pm.h"
+
+
+
+#define blue_led (1<<2)
+
+
+
+void gpio_portf_interrupt(void){
+    GPIO_PORTF_ICR_R=1;//isr is finished here
+    GPIO_PORTF_DATA_R^= blue_led;
+    //GPIO_PORTF_ICR_R=1;isr is again called and toggles the led.
+}
+void main(void){
+    SYSCTL_RCGC2_R   |= 0x00000020;      // ENABLE CLOCK TO GPIOF
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;      // UNLOCK COMMIT REGISTER
+    GPIO_PORTF_CR_R   = 0x0F;            // MAKE PORTF0 CONFIGURABLE
+    GPIO_PORTF_DEN_R  = 0x0F;            // SET PORTF PINS 4 PIN
+    GPIO_PORTF_DIR_R  = 0x0E;            // SET PORTF4 PIN AS INPUT USER SWITCH PIN
+    GPIO_PORTF_PUR_R  = 0x11;            // PORTF4 IS PULLED UP
+    GPIO_PORTF_DATA_R = 0x00;
+
+    //GPIO interrupt mask
+    GPIO_PORTF_IM_R = 0;
+    GPIO_PORTF_IS_R = 0;
+    GPIO_PORTF_IEV_R =1;
+    GPIO_PORTF_ICR_R=1;
+    GPIO_PORTF_IM_R = 1;
+
+    //NVIC
+
+    NVIC_EN0_R|=1<<30;
+
+    while(1){
+        //do nothing
+    }
+
+}
+
+
 
 

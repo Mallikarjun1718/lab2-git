e8fd0797dc166248a85e943c82e496657e2f1368
Mallikarjun S Biradar <220020034@iitdh.ac.in>
2025-10-07 11:18:41 +0530

    lab8 code added

--- main.c
+++ main.c
@@ -3,47 +3,45 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 
-#define red_led (1<<1)
-#define blue_led (1<<2)
 
-
-
-void gpio_portf_interrupt(void){
-    GPIO_PORTF_ICR_R=1;//isr is finished here
-    GPIO_PORTF_DATA_R^= blue_led;
-    //GPIO_PORTF_ICR_R=1;isr is again called and toggles the led.
-}
-
-void timer (void){
-    GPIO_PORTF_DATA_R^= red_led;
-}
-int main(void){
+void main(void){
+    int count=0,i;
     SYSCTL_RCGC2_R   |= 0x00000020;      // ENABLE CLOCK TO GPIOF
+    SYSCTL_RCGCTIMER_R |= (1 << 1);   // Enable Timer0 clock
     GPIO_PORTF_LOCK_R = 0x4C4F434B;      // UNLOCK COMMIT REGISTER
-    GPIO_PORTF_CR_R   = 0x0F;            // MAKE PORTF0 CONFIGURABLE
-    GPIO_PORTF_DEN_R  = 0x0F;            // SET PORTF PINS 4 PIN
+    GPIO_PORTF_CR_R   = 0x1F;            // MAKE PORTF0 CONFIGURABLE
+    GPIO_PORTF_DEN_R  = 0x1F;            // SET PORTF PINS 4 PIN
     GPIO_PORTF_DIR_R  = 0x0E;            // SET PORTF4 PIN AS INPUT USER SWITCH PIN
     GPIO_PORTF_PUR_R  = 0x11;            // PORTF4 IS PULLED UP
     GPIO_PORTF_DATA_R = 0x00;
 
-    NVIC_ST_RELOAD_R=0xFFFFFF;
-    NVIC_ST_CTRL_R=(1<<2 | 1<<1 | 1);
+    GPIO_PORTF_AFSEL_R = 0X04;
+    GPIO_PORTF_PCTL_R = 0X700;
 
-    //GPIO interrupt mask
-    GPIO_PORTF_IM_R = 0;
-    GPIO_PORTF_IS_R = 0;//edge triggered
-    GPIO_PORTF_IEV_R =1;//falling edge as when pressed it goes from 1 to 0
-    GPIO_PORTF_ICR_R=1;
-    GPIO_PORTF_IM_R = 1;
+    TIMER1_CTL_R = 0X00;
+    TIMER1_CFG_R = 0X04;
+    TIMER1_TAMR_R = 0X0A;
 
-    //NVIC
+    TIMER1_CTL_R &= ~(1<<6);
+    TIMER1_TAPR_R = 0X0;
 
-    NVIC_EN0_R|=1<<30;
+    TIMER1_TAILR_R = 0X140;
+    TIMER1_TAMATCHR_R = 0XA0;
+    TIMER1_CTL_R |= 0X01;
 
     while(1){
-        //do nothing
+        if((GPIO_PORTF_DATA_R & 0x01)== 0){
+            if(TIMER1_TAMATCHR_R<TIMER1_TAILR_R-0xF)
+                TIMER1_TAMATCHR_R += 0X0F;
+            for(i=0;i<500000;i++);
+        }
+        if((GPIO_PORTF_DATA_R & 0x10)== 0){
+            if(TIMER1_TAMATCHR_R > 0x0F)
+                TIMER1_TAMATCHR_R -= 0X0F;
+            for(i=0;i<500000;i++);
+        }
     }
-
 }
+
 
 
